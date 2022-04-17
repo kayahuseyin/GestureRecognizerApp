c085af8dc266eefe1f812dcc81143923cd0e1628
Hüseyin Kaya <kkayahuseyin@hotmail.com>
2022-04-18 01:50:19 +0300

    file upload

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var isScott = true // fonksiyon içinde oluşturulursa her tuşa basıldığında true olacaktır.
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic)) //event handler
+        //bir veya daha fazla tıklamayı algılar
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
-
+    
+    @objc func changePic() {
+        
+        if myLabel.text == "Michael Scott"{
+            imageView.image = UIImage(named: "halpert")
+            myLabel.text = "Jim Halpert"
+        } else {
+                imageView.image = UIImage(named: "scott")
+            myLabel.text = "Michael Scott"
+            }
+        /* Öbür yöntem
+         if isJames == true{
+         imageView.image = UIImage(named: "halpert")
+         myLabel.text = "Jim Halpert"
+         isScott = false
+         }else{
+         imageView.image = UIImage(named: "scott")
+         myLabel.text = "Michael Scott"
+         isJames = true}
+         */
+        
+    }
+    
 
 }
 
